4dd1d9519e97fe9a744aa79ef1c53cf1ed86bdb0
shahin2525 <shahin451999@gmail.com>
2025-02-27 09:44:41 -0800

    add private route

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,29 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+      {session?.user && (
+        <div>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome To {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome To {session?.user?.email}
+          </h1>
+          <Image
+            className="mx-auto rounded-full"
+            alt="user image"
+            height={100}
+            width={100}
+            src={session?.user?.image}
+          />
+        </div>
+      )}
     </div>
   );
 };
