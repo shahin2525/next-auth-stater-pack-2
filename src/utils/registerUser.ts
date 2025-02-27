9645f76760eb9f452b03481bcbbe5a65a5bc75de
shahin2525 <shahin451999@gmail.com>
2025-02-27 10:16:57 -0800

    add register server action

---
+++ src/utils/registerUser.ts
@@ -0,0 +1,14 @@
+import { UserData } from "@/app/register/page";
+
+export const registerUser = async (data: UserData) => {
+  const res = fetch(`${process.env.BACKEND_URL}/register`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+    cache: "no-store",
+  });
+  const userInfo = (await res).json();
+  return userInfo;
+};
